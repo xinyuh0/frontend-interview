9d40aaed519a381b3a76c4aa3bec572112a3b55d
HU Xinyu <xinyuh723@gmail.com>
2024-04-07 01:02:09 +0900

    mercari

--- oa/mercari/special.ts
+++ oa/mercari/special.ts
@@ -1,8 +1,21 @@
 const shortestPath = (N: number, X: number, Y: number) => {
-  const m: Map<number, number> = new Map<number, number>();
-
-  // Create entries
-  for (let i = 1; i <= N; i++) {
-    m.set(i, 0);
+  if (X > Y) {
+    const tmp = X;
+    X = Y;
+    Y = tmp;
   }
+
+  const distances: number[] = new Array(N + 1).fill(0);
+
+  // The answer should add up to (N-1)*N/2
+  for (let i = 1; i <= N; i++) {
+    for (let j = i + 1; j <= N; j++) {
+      let dis = Math.min(j - i, Math.abs(X - i) + Math.abs(Y - j) + 1);
+      distances[dis]++;
+    }
+  }
+
+  return distances.slice(1);
 };
+
+console.log(shortestPath(5, 2, 3));
